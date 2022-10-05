05977aae4ef46ea908037b37707a0330b212e744
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-10-05 23:29:35 +0300

    ref

--- src/script/mod-2.js
+++ src/script/mod-2.js
@@ -523,22 +523,22 @@
 
 // ЗАВДАННЯ 32/32
 
-function includes(array, value) {
-  for (const element of array) {
-    if (element === value) {
-      return true;
-    }
-  }
-  return false;
-}
+// function includes(array, value) {
+//   for (const element of array) {
+//     if (element === value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
 
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(includes([1, 2, 3, 4, 5], 17));
-console.log(
-  includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter")
-);
-console.log(
-  includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus")
-);
-console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
-console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
+// console.log(includes([1, 2, 3, 4, 5], 3));
+// console.log(includes([1, 2, 3, 4, 5], 17));
+// console.log(
+//   includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter")
+// );
+// console.log(
+//   includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus")
+// );
+// console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
+// console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
