2eac32132fe6fcf62e264c645a9c435e06d20a97
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-10-14 01:30:58 +0300

    stud

--- src/script/mod-3.js
+++ src/script/mod-3.js
@@ -298,7 +298,7 @@
 // console.log(countProps({ name: "Mango", age: 2 }));
 // console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
 
-// ЗАВДАННЯ 14/41
+// ЗАВДАННЯ 15/41
 
 // const apartment = {
 //   descr: "Spacious apartment in the city center",
@@ -313,4 +313,235 @@
 // console.log(keys);
 // console.log(values);
 
-// ЗАВДАННЯ 15/41
+// ЗАВДАННЯ 16/41
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   let salari = Object.keys(salaries);
+//   console.log(salari);
+//   for (let salariValue of salari) {
+//     totalSalary += salaries[salariValue];
+//     console.log(salaries[salariValue]);
+//   }
+
+//   return totalSalary;
+// }
+
+// countTotalSalary({});
+// countTotalSalary({ mango: 100, poly: 150, alfred: 80 });
+// countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 });
+
+// console.log(countTotalSalary({}));
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+// ЗАВДАННЯ 17/41
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const color of colors) {
+//   //   console.log(color);
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+//   console.log(hexColors);
+//   console.log(rgbColors);
+// }
+
+// ЗАВДАННЯ 18/41
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   for (const product of products) {
+//     // console.log(product);
+//     if (productName === product.name) {
+//       return product.price;
+//     }
+//   }
+//   return null;
+// }
+
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("Grip"));
+// console.log(getProductPrice("Scanner"));
+// console.log(getProductPrice("Droid"));
+// console.log(getProductPrice("Engine"));
+
+// ЗАВДАННЯ 19/41
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+// function getAllPropValues(propName) {
+//   const allPropValues = [];
+//   for (const product of products) {
+//     const keys = Object.keys(product);
+
+//     for (const key of keys) {
+//       if (key === propName) {
+//         allPropValues.push(product[key]);
+//       }
+//       console.log(allPropValues);
+//     }
+//   }
+//   return allPropValues;
+// }
+
+// getAllPropValues("name");
+// getAllPropValues("quantity");
+// getAllPropValues("price");
+// getAllPropValues("category");
+
+// ЗАВДАННЯ 20/41
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   for (const product of products) {
+//     if (productName === product.name) {
+//       return product.price * product.quantity;
+//     }
+//   }
+//   return 0;
+// }
+
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Droid"));
+// console.log(calculateTotalPrice("Grip"));
+// console.log(calculateTotalPrice("Scanner"));
+
+// ЗАВДАННЯ 21/41
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const { yesterday, today, tomorrow } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log(highTemperatures);
+// console.log(meanTemperature);
+
+// ЗАВДАННЯ 22/41
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const {
+//   yesterday,
+//   today,
+//   tomorrow,
+//   icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+// } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log(highTemperatures);
+// console.log(meanTemperature);
+
+// ЗАВДАННЯ 23/41
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const {
+//   yesterday: highYesterday,
+//   today: highToday,
+//   tomorrow: highTomorrow,
+//   icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+// } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+// console.log(highTemperatures);
+// console.log(meanTemperature);
+
+// ЗАВДАННЯ 24/41
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const { hex, rgb } of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+// ЗАВДАННЯ 25/41
+
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Change code below this line
+
+const highToday = forecast.today.high;
+const lowToday = forecast.today.low;
+const todayIcon = forecast.today.icon;
+
+const highTomorrow = forecast.tomorrow.high;
+const lowTomorrow = forecast.tomorrow.low;
+const tomorrowIcon = forecast.tomorrow.icon;
+
+console.log(highToday);
+console.log(lowToday);
+console.log(todayIcon);
+
+console.log(highTomorrow);
+console.log(lowTomorrow);
+console.log(tomorrowIcon);
